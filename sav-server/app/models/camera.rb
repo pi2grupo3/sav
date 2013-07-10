560a8c56b6586a4e788e3a16f118e8d3ae62ef1b
Daniel Bucher + Vinícius Vieira <daniel.bucher88@gmail.com>
2013-07-10 19:03:15 -0300

    Added translade method to Camera and it's test.

--- sav-server/app/models/camera.rb
+++ sav-server/app/models/camera.rb
@@ -3,4 +3,16 @@ class Camera < ActiveRecord::Base
 
   attr_accessible :url, :current_position, :current_x_position,
    :current_y_position, :go_to_position, :go_to_x_position, :go_to_y_position
+
+  #update position when translading...
+  def translade
+    direction = self.go_to_position
+    if direction == "left"
+      self.current_position -= 1
+    elsif direction == "right"
+      self.current_position += 1
+    end
+    self.go_to_position = "hold"
+    self.save    
+  end
 end
--- sav-server/test/unit/camera_test.rb
+++ sav-server/test/unit/camera_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class CameraTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ 
+  test "translade to right" do
+    cam = Camera.create!(:current_position => 0, :go_to_position => "right")
+    cam.translade
+    assert_equal 1, cam.current_position
+  end  
 end
