888af72a8fa6652ae9e5e9cf746e175a7f409b3c
Vinicius Vieira <vinicius@loopec.com.br>
2013-07-17 17:35:35 -0300

    upgrading rails4

--- sav-server/config/initializers/secret_token.rb
+++ sav-server/config/initializers/secret_token.rb
@@ -5,3 +5,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 SavServer::Application.config.secret_token = 'e1ec234f2df38bf3f0e484ce791d87f9c301a090ac12e0e1bde42b2a185a9dea0fb4b71151ac7b86f0e231f7a0610d20bd39e8ca4b1069ef66018c809e2bc8e2'
+
+SavServer::Application.config.secret_key_base = 'xxxe1ec234f2df38bf3f0e484ce791d87f9c301a090ac12e0e1bde42b2a185a9dea0fb4b71151ac7b86f0e231f7a0610d20bd39e8ca4b1069ef66018c809e2bc8e2'
