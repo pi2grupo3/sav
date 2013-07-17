7775ccd623dea84bd8d71770dfd4f986d7a4c99d
Daniel Bucher <daniel.bucher88@gmail.com>
2013-07-17 19:17:03 -0300

    created can_go_left? and can_go_right?

--- sav-server/config/routes.rb
+++ sav-server/config/routes.rb
@@ -59,7 +59,7 @@ SavServer::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  #root :to => 'cameras#index'
 
   # See how all your routes lay out with "rake routes"
 
