79477a1cfbb97bdba58c5bcfb03d97480150fb03
Daniel Bucher <daniel.bucher88@gmail.com>
2013-07-18 08:19:01 -0300

    Changed root_to page...

--- sav-server/config/routes.rb
+++ sav-server/config/routes.rb
@@ -4,6 +4,7 @@ SavServer::Application.routes.draw do
   get "welcome/seguranca"
   get "welcome/poste"
   get "welcome/monitoramento"
+
   resources :cameras do
     member do
       get 'translade'
@@ -63,7 +64,7 @@ SavServer::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
