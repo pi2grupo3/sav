cb5c5c1ee65312bea4d4d04bcf44d402dca35e06
carlosneo <carlos.neo@gmail.com>
2013-07-18 00:00:15 -0300

    adding new frontend vier. Avaiable at welcome/index

--- sav-server/config/routes.rb
+++ sav-server/config/routes.rb
@@ -1,5 +1,9 @@
 SavServer::Application.routes.draw do
 
+  get "welcome/index"
+  get "welcome/seguranca"
+  get "welcome/poste"
+  get "welcome/monitoramento"
   resources :cameras do
     member do
       get 'translade'
@@ -59,7 +63,7 @@ SavServer::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
--- sav-server/db/schema.rb
+++ sav-server/db/schema.rb
@@ -9,11 +9,11 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130603224125) do
+ActiveRecord::Schema.define(version: 20130603224125) do
 
-  create_table "cameras", :force => true do |t|
+  create_table "cameras", force: true do |t|
     t.string   "url"
     t.string   "current_position"
     t.string   "go_to_position"
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(:version => 20130603224125) do
     t.float    "current_y_position"
     t.float    "go_to_x_position"
     t.float    "go_to_y_position"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
